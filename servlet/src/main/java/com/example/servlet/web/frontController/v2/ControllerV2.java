0171b364c17c8279808d81d4a038ee7460714a6a
Doritosch <als000825@naver.com>
2025-05-06 20:17:34 +0900

    spring mvc

---
+++ servlet/src/main/java/com/example/servlet/web/frontController/v2/ControllerV2.java
@@ -0,0 +1,14 @@
+package com.example.servlet.web.frontController.v2;
+
+import com.example.servlet.web.frontController.MyView;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public interface ControllerV2 {
+
+    MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+
+}
---
+++ servlet/src/main/java/com/example/servlet/web/frontController/v3/ControllerV3.java
@@ -0,0 +1,10 @@
+package com.example.servlet.web.frontController.v3;
+
+import com.example.servlet.web.frontController.ModelView;
+
+import java.util.Map;
+
+public interface ControllerV3 {
+
+    ModelView process(Map<String, String> paramMap);
+}
